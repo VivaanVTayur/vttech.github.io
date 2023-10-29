5d9279bed5e658887fbcae320018eee1b69227c7
VivaanVTayur <148752346+VivaanVTayur@users.noreply.github.com>
2023-10-29 11:25:36 +0530

    Add files via upload

---
+++ noCircuit.js
@@ -0,0 +1,3 @@
+function noCircuit() {
+    alert("The circuit diagram is not shown as the video doesn't contain a circuit and that is because it is a how to use video or it is a programming video. It may also state inbuilt circuits such as the LED_BUILTIN in Arduino");
+}
